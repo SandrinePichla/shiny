72b4f3ff2c1ced4cfc90da3a47bea6ff64217376
SandrinePichla <sandrinepichla@hotmail.com>
2025-07-08 17:18:37 +0200

    styled-components Balloon

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react"
+import styled from "styled-components"
+
+const HomeContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+const Balloon = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background-color: red;
+  transform: scale(${({size}) => size});
+`
 function Home() {
+  const [size, setSize] = useState(1)
   return (
-    <div>
-      <h1> Page d'accueil 🏠</h1>
-    </div>
+    <HomeContainer>
+      <h1 onClick={() => setSize(size + 0.1)}> Page d'accueil 🏠</h1>
+      <Balloon size={size} />
+    </HomeContainer>
   )
 }
 
